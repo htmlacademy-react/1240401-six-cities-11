79a9000252d9230b0ed8492f6d7adfe1799cac03
Anna <aventsel.15@gmail.com>
2022-10-29 11:29:51 +0200

    routing

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
 import Main from '../../pages/main/main';
-
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import Room from '../../pages/room/room';
+import UnexistingPage from '../../pages/page404/page404';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import PrivateRoute from '../private-route/private-route';
 
 type AppOfferProps = {
   offersCount: number;
@@ -8,7 +14,36 @@ type AppOfferProps = {
 
 function App({offersCount}: AppOfferProps): JSX.Element{
   return (
-    <Main offersCount={offersCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          path={AppRoute.Main}
+          element={<Main offersCount={offersCount} />}
+        </Route>
+        <Route>
+          path={AppRoute.Login}
+          element={<Login />}
+        </Route>
+        <Route>
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <Favorites />
+            </PrivateRoute>
+          }
+        </Route>
+        <Route>
+          path={AppRoute.Room}
+          element={<Room />}
+        </Route>
+        <Route>
+          path=`*`
+          element={<UnexistingPage />}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/pages/page404/page404.tsx
@@ -0,0 +1,12 @@
+function UnexistingPage():JSX.Element {
+  return (
+    <>
+      <h1>Ошибка 404</h1>
+      <p>Страница не найдена!</p>
+      <link rel="stylesheet" href="#todo">Перейти на главную страницу</link>
+    </>
+  );
+}
+
+export default UnexistingPage;
+
